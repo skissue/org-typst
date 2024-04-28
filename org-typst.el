f2872deea3bf2a5f938970505dd9c2b030d2c489
Ad <144847922+skissue@users.noreply.github.com>
2024-04-27 20:31:26 -0400

    feat: add babel functionality
    Ported from https://github.com/Cj-bc/ob-typst ❤️

--- org-typst.el
+++ org-typst.el
@@ -28,6 +28,73 @@
 
 ;;; Code:
 
+;; Babel functionality is ported from https://github.com/Cj-bc/ob-typst
+(defconst org-typst--babel-settable-elements
+  '(bibliography list cite document emph figure footnote heading link
+    enum numbering outline par parbreak quote ref strong table terms
+    page)
+  "List of settable elements.")
+
+(defcustom org-typst-babel-default-rules '((page . "width: auto, height: auto, margin: 0.3em"))
+  "Alist of default Typst set rules that will be prepended to all Typst code.
+Each rule will be set only if the Typst snippet doesn't already
+set a rule for that element."
+  :type `(alist :key-type (choice ,@(mapcar #'(lambda (e) `(const ,e)) org-typst--babel-settable-elements))
+              :value-type string))
+
+(defun org-typst--babel-rules-in-string (body)
+  "Return a list of elements that have 'set' rules applied in the
+ Typst markup BODY."
+  (seq-reduce
+   (lambda (acc s)
+     (let ((rule (save-match-data
+                    (string-match (rx (: line-start "#set " (group (+ (not "("))) "(")) s)
+                    (match-string 1 s))))
+       (if rule (cons rule acc) acc)))
+   (string-lines body) '()))
+
+(defun org-typst--babel-get-default-rules (body)
+  "Return formatted code that sets rules from
+ `org-typst-babel-default-rules' as needed."
+  (string-join (mapcar (lambda (c)
+                         (unless (member (symbol-name (car c))
+                                         (org-typst--babel-rules-in-string body))
+                           (format "#set %s(%s)" (car c) (cdr c))))
+                       org-typst-babel-default-rules)
+               "\n"))
+
+(defun org-typst--babel-create-image (body tofile)
+  "Create an image from Typst source using external process.
+
+The Typst markup BODY is saved to a temporary Typst file, then converted to an
+image file using 'typst compile'.
+
+The generated image file is eventually moved to TOFILE.
+
+Generated file format is determined by TOFILE file extension. Supported file
+formats are png, pdf, and svg."
+  (unless (executable-find "typst")
+    (user-error "No 'typst' executable found!"))
+  (let* ((tmpfile (org-babel-temp-file "ob-typst-src"))
+         (ext (file-name-extension tofile))
+         (log-buf (get-buffer-create "*Org Typst Output*"))
+         (default-rules (org-typst--babel-get-default-rules body)))
+    (with-temp-file tmpfile
+      (insert default-rules "\n\n" body))
+    (copy-file (org-compile-file
+                tmpfile
+                (list (format "typst compile --format %s %%F %%O" ext))
+                ext "" log-buf)
+               tofile 'replace)))
+
+;;;###autoload
+(defun org-babel-execute:typst (body params)
+  "Execute a block of Typst markup."
+  (let* ((out-file (or (alist-get :outfile params)
+                       (org-babel-temp-file "ob-typst-out" (format ".%s" ob-typst/default-format)))))
+    (org-typst--babel-create-image body out-file)
+    out-file))
+
 (provide 'org-typst)
 
 ;;; org-typst.el ends here
